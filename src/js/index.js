1ff36bb70c7deb654a19b4fd2f8f7d5f8be1e9a2
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-09-09 19:06:20 +0000

    Avance

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,31 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+const SimpleCounter = (props) => {
+    return (<div className="Counter">
+         <div className="clock"><FontAwesomeIcon icon={faClock} /></div>
+         <div className="six">0</div>
+         <div className="five">0</div>
+         <div className="four">0</div>
+         <div className="three">0</div>
+         <div className="two">0</div>
+         <div className="one">0</div>
+         
+
+    </div>);
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<SimpleCounter />);
 
