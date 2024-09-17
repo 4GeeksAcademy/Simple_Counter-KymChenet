7816e647d28017a8184481509f6da523ee28158b
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-09-17 21:30:26 +0000

    avance

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {createRoot} from "react-dom/client";
 import "../styles/index.css";
 import Home from "./component/home";
@@ -7,45 +7,66 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 import PropTypes from 'prop-types';
 
-const SecondCounter = ({digitSix, digitFive, digitFour, digitThree, digitTwo, digitOne}) => {
+
+
+const SecondCounter = () => {
+
+
+    const [ active, setActive]  = useState(false)
+    const [seconds, setSeconds] = useState(0)
+
+
+    useEffect(( ) => {
+        let intervalId;
+
+        if(active) {
+            intervalId = setInterval(() => {
+                setSeconds(value => value +1) 
+                },1000)
+            } else {
+                clearInterval(intervalId)
+
+            }
+            return () => clearInterval(intervalId)    
+    }, [active]);
+
+    const startStop = () => setActive(value => value=!value)
+    const resetTimer = () => setActive(value => seconds = 0)    
     return (
         <div className="bigCounter"> 
-            <div className="clock" ><FontAwesomeIcon icon={faClock} /></div>
-            <div className="six">{digitSix %10 }</div>
-            <div className="five">{digitFive %10 }</div>
-            <div className="four">{digitFour %10 }</div>
-            <div className="three">{digitThree %10 }</div>
-            <div className="two">{digitTwo %10 }</div>
-            <div className="one">{digitOne %10 }</div>
+                <section className="counter-head">
+                    <div className="clock" ><FontAwesomeIcon icon={faClock} /></div> 
+                    <div>{Math.floor(seconds/100000)%10}</div> 
+                    <div>{Math.floor(seconds/10000)%10}</div> 
+                    <div>{Math.floor(seconds/1000)%10}</div> 
+                    <div>{Math.floor(seconds/100)%10}</div> 
+                    <div>{Math.floor(seconds/10)%10}</div> 
+                    <div>{Math.floor(seconds)%10}</div> 
+                </section>
+                
+                <section  className= "container text-center">
+                        <button onClick={startStop} className="mx-2 btn mx-2 btn-success">Start</button>
+                        <button onClick={startStop} className="mx-2 btn mx-2 btn-danger">Stop</button>
+                        <button onClick={resetTimer} className="mx-2 btn mx-2 btn-primary">Reset</button>
+                </section>
+
+            
 
         </div>
         
     )
-      
+       
 }
 
-SecondCounter.propTypes = {
- digitSix: PropTypes.number,
- digitFive: PropTypes.number,
- digitFour: PropTypes.number,
- digitThree: PropTypes.number,
- digitTwo: PropTypes.number,
- digitOne: PropTypes.number
-};
 
-let counter = 0;
+
 
 const app = createRoot(document.getElementById('app'))
+let counter = 0;
 
 setInterval(() => {
-   const six = Math.floor(counter/100000);
-   const five = Math.floor(counter/10000);
-   const four = Math.floor(counter/1000);
-   const three = Math.floor(counter/100);
-   const two = Math.floor(counter/10);
-   const one = Math.floor(counter/1);
 
     counter++;
-    app.render(<SecondCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>)
+    app.render(<SecondCounter seconds={counter}/>)
 }, 1000);
 
